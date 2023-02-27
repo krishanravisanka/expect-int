df17273e1c5a98fda752b65d0c94307f5cc7cf94
Krishan Ravisanka <krishanrs20@gmail.com>
2023-02-27 12:06:42 +0000

    add interactive method and expect string

--- sftp_automation.sh
+++ sftp_automation.sh
@@ -4,9 +4,9 @@
 spawn sftp test-user@ipaddress
 expect "Enter your LAB  password:"
 send "password\r"
-expect "Press Enter to Continue."
 send "\r"
 
 # Expect SFTP prompt and send command
 expect "sftp>"
 send "ls\r"
+interact
